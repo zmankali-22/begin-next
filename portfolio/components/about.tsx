b7a67a2dd536f6f2a0f66bb8e5bbff2c642c938b
utsav <sav.acharya22@gmail.com>
2024-05-30 03:00:25 +1000

    section heading

---
+++ portfolio/components/about.tsx
@@ -0,0 +1,41 @@
+/* eslint-disable react/no-unescaped-entities */
+import React from "react";
+import SectionHeading from "./section-heading";
+
+export default function About() {
+  return (
+    <section className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40">
+      <SectionHeading>About Me</SectionHeading>
+      <p className="mb-3">
+        After working in {" "}
+        <span className="font-medium">Hospitality</span> for years, I decided to
+        pursue my passion for programming. I enrolled in <span className="font-medium">Coder Academy </span> and learned{" "}
+        <span className="font-medium">
+          full-stack web development
+        </span>
+        .{" "}
+        <span className="italic">
+          My favorite part of programming
+        </span>{" "}
+        is the problem-solving aspect. I{" "}
+        <span className="underline">love</span> the feeling of finally
+        figuring out a solution to a problem. I have learnt many exciting technologies namely {" "}
+        <span className="font-medium"> HTML, CSS, Javascript, Python, Flask,
+          React, Node.js, MongoDB
+        </span>
+        . I am also familiar with TypeScript and NextJS. I am always
+        looking to learn new technologies. I am currently looking for
+        a <span className="font-medium">full-time position</span> as a
+        software developer.
+      </p>
+
+      <p>
+        <span className="italic">When I'm not coding</span>, I enjoy
+        playing video games, watching movies, and cooking food.
+        I also line to go for a run outdoors to {" "}
+        <span className="font-medium">enjoy the nature</span>
+       
+      </p>
+    </section>
+  );
+}
---
+++ portfolio/components/intro.tsx
@@ -0,0 +1,105 @@
+/* eslint-disable react/no-unescaped-entities */
+"use client";
+
+import Image from "next/image";
+import { motion } from "framer-motion";
+import Link from "next/link";
+import { BsArrowRight, BsGithub, BsLinkedin } from "react-icons/bs";
+import { HiDownload } from "react-icons/hi";
+import { FaGithubSquare } from "react-icons/fa";
+
+export default function Intro() {
+  return (
+    <section className="mb-28 max-w-[50rem] text-center sm:mb-0">
+      <div className="flex items-center justify-center">
+        <div className="relative">
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: "tween",
+              duration: 0.2,
+            }}
+          >
+            <Image
+              src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?fit=crop&w=368&h=368&q=100"
+              alt="Sav portrait"
+              width="192"
+              height="192"
+              quality="95"
+              priority={true}
+              className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
+            />
+          </motion.div>
+          <motion.span
+            className="absolute bottom-0 right-0 text-3xl"
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: "spring",
+              stiffness: 125,
+              delay: 0.1,
+              duration: 0.7,
+            }}
+          >
+            👋
+          </motion.span>
+        </div>
+      </div>
+
+      <motion.h1
+        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+      >
+        <span className="font-bold">Hello, I'm Utsav.</span> I'm a{" "}
+        <span className="font-bold">Coder Academy</span> graduate with{" "}
+        <span className="font-bold">passion</span> for web
+        development. I enjoy building{" "}
+        <span className="italic">sites & apps</span>. I like{" "}
+        <span className="underline">React (Next.js)</span>.
+      </motion.h1>
+
+      <motion.div
+        className="flex flex-col sm:flex-row justify-center items-center gap-2 px-4 text-lg font-medium "
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.1,
+        }}
+      >
+        <Link
+          href="#contact"
+          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+        >
+          Contact me here{" "}
+          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
+        </Link>
+
+        <a
+          href="/cv.pdf"
+          className="group bg-white  px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-105 hover:scale-105 active:scale-105 transition border border-black/10"
+        >
+          Download CV{" "}
+          <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+        </a>
+
+        <a
+          href="https://linkedin.com/"
+          target="_blank"
+          className="bg-white p-4  text-gray-700 flex items-center gap-2 rounded-full  focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition border border-black/10"
+        >
+          <BsLinkedin />{" "}
+        </a>
+
+        <a
+          href="https://github.com/"
+          target="_blank"
+          className="bg-white p-4  text-gray-700 flex items-center gap-2 rounded-full  focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition border border-black/10"
+        >
+          <FaGithubSquare />{" "}
+        </a>
+      </motion.div>
+    </section>
+  );
+}
